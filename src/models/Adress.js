b7cc214ed7e3fa2d6b0b66ccb92c1623bb02b25f
Julio Cesar Rocha <julio.1009@hotmail.com>
2022-04-18 16:13:19 -0300

    [+] Adress model add

---
+++ src/models/Adress.js
@@ -0,0 +1,30 @@
+const mongooose = require('mongoose');
+
+const adressSchema = new mongooose.Schema({
+    cep: {
+        type: String,
+        required: true,
+        match: [/^[0-9]{8}$/, "CEP must be 8 numbers"],
+    },
+    street: {
+        type: String,
+        required: true,
+    },
+    district: {
+      type: String,
+      required: true,
+    },
+    number: {
+        type: Number,
+        required: true,
+    },
+    complement: {
+        type: String,
+        required: false,
+    },
+});
+
+const adress = mongooose.model('adresses', adressSchema);
+export default adress;
+
+    
