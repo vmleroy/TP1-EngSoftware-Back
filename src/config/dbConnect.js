72c5f90f392cb25728ce7d1bc941f343f01bf5f9
Julio Cesar Rocha <julio.1009@hotmail.com>
2022-04-23 14:22:44 -0300

    MongoDB_URI to string

--- src/config/dbConnect.js
+++ src/config/dbConnect.js
@@ -2,7 +2,7 @@ import mongoose from "mongoose";
 import dotenv from "dotenv";
 dotenv.config();
 
-mongoose.connect(process.env.MONGODB_URI)
+mongoose.connect(process.env.MONGODB_URI.toString())
 
 let db = mongoose.connection
 export default db
