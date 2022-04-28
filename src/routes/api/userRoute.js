870548d53ee19f60c0a7b1b5de5a9535f3eacd9d
Puraneshi <rsnd.leo@gmail.com>
2022-04-28 00:15:38 -0300

    Adiciona rota do login

--- src/routes/api/userRoute.js
+++ src/routes/api/userRoute.js
@@ -6,8 +6,12 @@ const router = express.Router();
 router 
     .get("/usuario", UserController.getUsers)
     .get("/usuario/:id", UserController.getUserById)
+
     .post("/usuario", UserController.createUser)
+    .post("/login", UserController.loginUser)
+
     .put("/usuario/:id", UserController.updateUser)
+
     .delete("/usuario/:id", UserController.deleteUser)
     
 export default router;
