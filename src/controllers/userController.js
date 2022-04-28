634eb221aaec0f22d7f2e6e2cbf59b248b8d2157
Pablo Felipe <pablofelipe1999@hotmail.com>
2022-04-28 07:38:42 -0300

    Changing password hashing from 'md5' to 'bcrypt' and modifying user register and user login methods

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -1,7 +1,11 @@
 import user from '../models/User.js'
-import md5 from 'md5'
+import bcrypt from "bcrypt"
 import dotenv from 'dotenv'
 dotenv.config();
+
+// Declaring a "cost factor" to control the um needed time to calculate the password hashing.
+// The saltRounds is used by the bcrypt dependency as a parameter to generate the hashed password.
+const saltRounds = 10;
 
 class UserController {
   static getUsers = (req, res) => {
@@ -14,7 +18,7 @@ class UserController {
   }
 
   static getUserById = (req, res) => {
-    user.findById(req.params.id, (err, user) => {
+    user.findByIdAndRemove(req.params.id, (err, user) => {
       if (err)
         res.status(400).send({message: `${err.message} - falha ao requisitar usuário.`})
       else
@@ -25,25 +29,42 @@ class UserController {
   static createUser = (req, res) => {
     const newUser = new user(req.body)
     newUser.roles.push('user')
-    newUser.password = md5(newUser.password + process.env.SALT_KEY)
-    newUser.save((err, user) => {
-      if (err)
-        res.status(400).send({message: `${err.message} - falha ao criar usuário.`})
+
+    // The user password will be hashed before being stored in the DB, according to the saltRounds:
+    bcrypt.hash(newUser.password, saltRounds, (err, hash) => {
+      if (err) {
+        res.status(400).send({message: `${err.message} - falha ao criar senha.`})
+      }
       else
-        res.status(200).send({ message: `Usuario criado com sucesso!`})
+        // If everything is ok, then newUser.password receives the hashed password:
+        newUser.password = hash
+        newUser.save((err, user) => {
+          if (err)
+            res.status(400).send({message: `${err.message} - falha ao criar usuário.`})
+          else
+            res.status(200).send({ message: `Usuario criado com sucesso!`})
+        })
     })
   }
 
   static userLogin = (req, res) => {
-    const userEmail = req.body.email
-    const userPassword = req.body.password
+    // Getting userEmail and userPassword from the POST login request:
+    const { email, password } = req.body
 
-    user.findOne({ email: userEmail }, (err, user) => {
+    // Find the user according to the email used in the login:
+    user.findOne({ email: email }, (err, user) => {
       if (err)
         res.status(401).send({message: `${err.message} - nao autorizado.`})
-      else
-        if (user.password == userPassword)
-          res.status(200).json(user)
+      else {
+        // If the user is found, then we need to validate the password:
+        bcrypt.compare(user.password, password, function(err, result) {
+          if (err)
+            res.status(401).send({message: `${err.message} - nao autorizado.`})
+          else
+            // If the passwords match, login password is correct so return the user:
+            res.status(200).json(user)
+        })
+      }
     })
   }
 
